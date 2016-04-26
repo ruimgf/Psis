49a897237c22394319a16d18978801efc983328f
Jorge Sacadura <jorgesacadura@Jorges-MacBook-Pro.local>
2016-04-26 19:08:56 +0100

    insert list ja com o overwrite

--- list.c
+++ list.c
@@ -12,17 +12,35 @@ item * creat_list(){
     return NULL;
 }
 
-item *  insert_begin_list(item * begin, unsigned int key,char * value){
-
-     item * aux;
-     item * new;
-
-     new = (item *) malloc(sizeof(item));
-     new->key  = key;
-     new->value = (char *) malloc(sizeof(value)) ;
-     strcpy(new->value, value);
-     new->next = begin;
-     return new;
+int insert_begin_list(item * *begin, unsigned int key,char * value,int overwrite)
+{
+  item * aux = begin;
+  item * new;
+  //procura se a key ja existe
+  while(aux->key!=key)
+    aux=aux->seg;
+  //se encontrou essa key
+  if(aux->key==key)
+  {
+    if(overwrite==1)
+    {
+      aux->value=value;
+      return 2; //fez overwrite
+    }
+  }
+  //se nao encontrou a key vai criar o novo
+  else
+  {
+    aux=begin;
+    new = (item *) malloc(sizeof(item));
+    new->key  = key;
+    new->value = (char *) malloc(sizeof(value));
+    strcpy(new->value, value);
+    new->next = aux;
+    *begin=new;
+    return 1; //criou um novo e inseriu
+  }
+  return 0; //se nao inseriu nem fez overwrite
 }
 
 item *  search_key_on_list(item * begin, unsigned int key){
@@ -45,7 +63,7 @@ item *  search_key_on_list(item * begin, unsigned int key){
 
 }
 
-int delete_entry(item ** begin, int id)
+int delete_entry(item * *begin, int id)
 {
     item * aux1 = begin;
     item * aux2 = aux->seg;
@@ -76,7 +94,7 @@ void delete_list(item * begin){
     if(aux1 == NULL){
         return;
     }
-
+    
     while(aux1!=NULL){
       aux2 = aux1->next;
       free(aux1->value);
@@ -84,7 +102,6 @@ void delete_list(item * begin){
       aux1=aux2;
     }
 }
-
 
 void print_list(item * begin){
     item * aux = begin;
